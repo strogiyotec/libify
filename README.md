bf86f854cdda2c59086963751519b74a51e66211
strogiyotec <almas337519@gmail.com>
2021-03-03 21:29:37 -0800

    add show command skeleton

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # libify
 Save deadline date of borrowed books into Google calendar
+
+# Motivation
+I often borrow books from **Vancouver Public Library** or **New West Public Library** 
+but I don't remember the holding expriration date.
+
+**Libify** provides a cli to save all deadlines into your google calendar
---
+++ cmd/show.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// showCmd represents the show command
+var showCmd = &cobra.Command{
+	Use:   "show",
+	Short: "Show a list of borrowed books",
+	Long: ` The usage:
+
+    libify show --password 123 --username almas`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Password %s and User %s\n", password, username)
+	},
+}
+var (
+	password string
+	username string
+)
+
+func init() {
+	rootCmd.AddCommand(showCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// showCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	showCmd.PersistentFlags().StringVarP(&password, "password", "p", "", "Profile password")
+	showCmd.PersistentFlags().StringVar(&username, "username", "", "Username")
+}
