22a77d129c90d2636e17da5e9f97ab527381360f
strogiyotec <almas337519@gmail.com>
2021-03-12 20:43:10 -0800

    post with custom headers

--- handlers/show_books.go
+++ handlers/show_books.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -41,19 +42,33 @@ func getCsrfToken() string {
 
 func (cred *LibraryCredentials) login(token string) {
 	fmt.Println("Send request")
-	response, err := http.PostForm(LOGIN_URL, url.Values{
+
+	values := url.Values{
 		"utf8":               {"%E2%9C%93"},
 		"authenticity_token": {token},
 		"name":               {cred.Username},
 		"user_pin":           {cred.Password},
 		"remember_me":        {"false"},
-		"local":              {"false"}})
+		"local":              {"false"}}
+	req, err := http.NewRequest("POST", LOGIN_URL, bytes.NewBufferString(values.Encode()))
 	if err != nil {
 		panic("Error during marshaling")
 	}
-	defer response.Body.Close()
-	fmt.Printf("Headers %v", response.Header)
-	body, err := ioutil.ReadAll(response.Body)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
+	req.Header.Set("Host", "newwestminster.bibliocommons.com")
+	req.Header.Set("X-CSRF-Token", token)
+	req.Header.Set("Origin", "https://newwestminster.bibliocommons.com")
+	req.Header.Set("Referer", "https://newwestminster.bibliocommons.com/user/login?destination=https%3A%2F%2Fnewwestminster.bibliocommons.com%2F")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	fmt.Printf("Headers %v", resp.Header)
+	fmt.Printf("Response code %d\n", resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic("Can't read body")
 	}
