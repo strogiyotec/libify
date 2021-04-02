49d1ee72c34cd60012e9e5e18a23a4b87e4c721a
strogiyotec <almas337519@gmail.com>
2021-04-01 18:33:19 -0700

    add google oauth

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
+	golang.org/x/oauth2 v0.0.0-20210313182246-cd4f82c27b84
 	google.golang.org/api v0.43.0 // indirect
 )
--- google/oauth.go
+++ google/oauth.go
@@ -1,12 +1,15 @@
 package google
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
-	"google.golang.org/api/oauth2/v2"
+	"google.golang.org/api/calendar/v3"
 )
 
 var (
@@ -14,7 +17,7 @@ var (
 		RedirectURL:  "http://localhost:3000/GoogleCallback",
 		ClientID:     os.Getenv("GOOGLE_KEY"),
 		ClientSecret: os.Getenv("GOOGLE_SECRET"),
-		Scopes:       []string{},
+		Scopes:       []string{"https://www.googleapis.com/auth/calendar"},
 		Endpoint:     google.Endpoint,
 	}
 	oauthRandom = "random"
@@ -26,12 +29,56 @@ const htmlIndex = `<html><body>
 `
 
 func CreateClient() {
-	http.HandlerFunc("/", handleMain)
-	http.HandlerFunc("/GoogleLogin", handleLogin)
-	http.HandlerFunc("/GoogleCallback", handleCallBack)
-	fmt.Pritnln(http.ListenAndServe(":3000", nil))
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/GoogleLogin", handleLogin)
+	http.HandleFunc("/GoogleCallback", handleCallBack)
+	fmt.Println(http.ListenAndServe(":3000", nil))
+}
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, htmlIndex)
 }
 
-func handleMain(writer http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, htmlIndex)
+func handleCallBack(w http.ResponseWriter, r *http.Request) {
+	state := r.FormValue("state")
+	if state != oauthRandom {
+		fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthRandom, state)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	code := r.FormValue("code")
+	token, err := googleOauthConf.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		fmt.Printf("oauthConf.Exchange() failed with '%s'\n", err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	client := oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(token))
+	service, err := calendar.New(client)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	events, err := service.
+		Events.
+		List("primary").
+		TimeMin(time.Now().
+			Format(time.RFC3339)).
+		MaxResults(5).
+		Do()
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	if len(events.Items) > 0 {
+		for _, i := range events.Items {
+			fmt.Fprintln(w, i.Summary, " ", i.Start.DateTime)
+		}
+	}
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConf.AuthCodeURL(oauthRandom)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
